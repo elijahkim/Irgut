734a2cba25da31f9e611c0358b5d1a70110f124d
Elijah Kim <kimelijah90@gmail.com>
2017-07-31 19:22:21 -0400

    Adds ability to evaluate arbitrary code

--- apps/irgut/lib/irgut.ex
+++ apps/irgut/lib/irgut.ex
@@ -6,4 +6,14 @@ defmodule Irgut do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def evaluate(code_string) do
+    try do
+      {result, binding} =
+        code_string
+        |> Code.eval_string
+    rescue
+      exception -> IO.inspect Exception.message(exception)
+    end
+  end
 end

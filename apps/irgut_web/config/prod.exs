5fb697b4041954fc062a6ab3e9b55f54a9b25544
Elijah Kim <kimelijah90@gmail.com>
2017-07-31 21:21:35 -0400

    Attempts to fix heroku sockets

--- apps/irgut_web/config/prod.exs
+++ apps/irgut_web/config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :irgut_web, IrgutWeb.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "irgut.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "irgut.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
